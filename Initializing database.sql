b5bc46acde396dc2c323a102b7350651c25bd964
Anika Saxena <anikasaxena0804@gmail.com>
2026-06-29 00:35:42 +0530

    Database initialization script
    Added comments to explain the script's purpose and structure.

--- Initializing database.sql
+++ Initializing database.sql
@@ -1,6 +1,16 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' 
+*/
+
+-- Create the 'DataWarehouse' database
 USE DataWarehouse;
 GO
 
+-- Create Schemas
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
