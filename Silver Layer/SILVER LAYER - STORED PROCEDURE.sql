22a35c5b924e1c9db856178c36181c7f8f906ac8
Anika Saxena <anikasaxena0804@gmail.com>
2026-06-29 00:27:20 +0530

    ETL(bronze -> silver layer)

--- Silver Layer/SILVER LAYER - STORED PROCEDURE.sql
+++ Silver Layer/SILVER LAYER - STORED PROCEDURE.sql
@@ -1,3 +1,17 @@
+/*
+===============================================================================
+Stored Procedure: ETL(bronze -> silver layer)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+===============================================================================
+*/
+
+
 EXEC Silver.load_silver;
 GO
 
@@ -6,10 +20,6 @@ BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
     BEGIN TRY
         SET @batch_start_time = GETDATE();
-        PRINT '================================================';
-        PRINT 'Loading Silver Layer';
-        PRINT '================================================';
-
 		PRINT '------------------------------------------------';
 		PRINT 'Loading CRM Tables';
 		PRINT '------------------------------------------------';
